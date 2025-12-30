4e717f44cff315d90495d24d5d55499a1cb958fb
Abhishek <dhamalaabhishek7@gmail.com>
2025-12-30 16:14:41 +0545

    auth: migrate login and register to clean architecture

---
+++ lib/features/auth/presentation/pages/register_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:sajilo_sewa/features/auth/presentation/widgets/register_form.dart';
+import 'package:sajilo_sewa/features/auth/presentation/widgets/register_header.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(children: [RegisterHeader(), RegisterForm()]),
+      ),
+    );
+  }
+}
