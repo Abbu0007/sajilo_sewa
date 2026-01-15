e604b79f18740e583164f154bbe51f5c07bab873
Abhishek <dhamalaabhishek7@gmail.com>
2026-01-15 20:10:29 +0545

    Update splash screen logic for improved startup navigation

--- lib/features/splash/presentation/pages/splash_screen.dart
+++ lib/features/splash/presentation/pages/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sajilo_sewa/app/routes/app_routes.dart';
+import 'package:sajilo_sewa/core/services/storage/user_session_service.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,8 +13,20 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+
     Future.delayed(const Duration(seconds: 2), () {
-      Navigator.pushReplacementNamed(context, AppRoutes.onboarding);
+      final session = UserSessionService.instance;
+
+      if (session.isLoggedIn()) {
+        Navigator.pushReplacementNamed(context, AppRoutes.main);
+        return;
+      }
+
+      if (session.isOnboardingDone()) {
+        Navigator.pushReplacementNamed(context, AppRoutes.login);
+      } else {
+        Navigator.pushReplacementNamed(context, AppRoutes.onboarding);
+      }
     });
   }
 
