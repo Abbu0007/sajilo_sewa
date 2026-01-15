c01e0e437e9cfd339451be654c1356046edb791c
Abhishek <dhamalaabhishek7@gmail.com>
2026-01-15 20:09:59 +0545

    Enhance reusable text field widget with validation and prefix support

--- lib/core/widgets/my_textformfield.dart
+++ lib/core/widgets/my_textformfield.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MyTextFormField extends StatelessWidget {
   final TextEditingController controller;
@@ -6,32 +7,60 @@ class MyTextFormField extends StatelessWidget {
   final IconData icon;
   final bool obscure;
 
+  // 🆕 New optional fields
+  final String? label;
+  final String? Function(String?)? validator;
+  final TextInputType? keyboardType;
+  final List<TextInputFormatter>? inputFormatters;
+  final Widget? prefix;
+  final Widget? suffix;
+
   const MyTextFormField({
     super.key,
     required this.controller,
     required this.hint,
     required this.icon,
     this.obscure = false,
+    this.label,
+    this.validator,
+    this.keyboardType,
+    this.inputFormatters,
+    this.prefix,
+    this.suffix,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      obscureText: obscure,
-      decoration: InputDecoration(
-        hintText: hint,
-        prefixIcon: Icon(icon, color: Colors.grey),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: const BorderSide(color: Colors.grey),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (label != null) ...[
+          Text(label!, style: const TextStyle(fontWeight: FontWeight.w600)),
+          const SizedBox(height: 6),
+        ],
+        TextFormField(
+          controller: controller,
+          obscureText: obscure,
+          validator: validator,
+          keyboardType: keyboardType,
+          inputFormatters: inputFormatters,
+          decoration: InputDecoration(
+            hintText: hint,
+            prefixIcon: Icon(icon, color: Colors.grey),
+            prefix: prefix,
+            suffixIcon: suffix,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: const BorderSide(color: Colors.grey),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: const BorderSide(color: Color(0xFFE8E8E8)),
+            ),
+            contentPadding: const EdgeInsets.symmetric(vertical: 14),
+          ),
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: const BorderSide(color: Color(0xFFE8E8E8)),
-        ),
-        contentPadding: const EdgeInsets.symmetric(vertical: 14),
-      ),
+      ],
     );
   }
 }
