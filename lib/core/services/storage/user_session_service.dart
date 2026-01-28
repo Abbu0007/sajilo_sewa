44eff0f5bc2f9a6b5707d52aa63197b6650a3cca
Abhishek Dhamala <dhamalaabhishek7@gmail.com>
2026-01-28 15:22:31 +0545

    Sprint 5: Persist auth token in user session service

--- lib/core/services/storage/user_session_service.dart
+++ lib/core/services/storage/user_session_service.dart
@@ -5,8 +5,9 @@ class UserSessionService {
   static final UserSessionService instance = UserSessionService._();
 
   static const _kLoggedIn = 'session_logged_in';
-  static const _kUserKey = 'session_user_key'; // store normalized email
-  static const _kRole = 'session_role'; // admin|client|provider
+  static const _kUserKey = 'session_user_key';
+  static const _kRole = 'session_role';
+  static const _kToken = 'session_token';
   static const _kOnboardingDone = 'onboarding_done';
 
   Future<void> init() async {
@@ -31,6 +32,7 @@ class UserSessionService {
     await StorageService.instance.setBool(_kLoggedIn, true);
     await StorageService.instance.setString(_kUserKey, userKey);
     await StorageService.instance.setString(_kRole, role);
+    await StorageService.instance.setString(_kToken, token);
   }
 
   bool isLoggedIn() {
@@ -39,10 +41,12 @@ class UserSessionService {
 
   String? getUserKey() => StorageService.instance.getString(_kUserKey);
   String? getRole() => StorageService.instance.getString(_kRole);
+  String? getToken() => StorageService.instance.getString(_kToken);
 
   Future<void> clearSession() async {
     await StorageService.instance.setBool(_kLoggedIn, false);
     await StorageService.instance.remove(_kUserKey);
     await StorageService.instance.remove(_kRole);
+    await StorageService.instance.remove(_kToken);
   }
 }
